9b6278c7c1946f59304fdde5567188c19aef66ac
yash312312 <yashsrivastava3@gmail.com>
2020-09-29 09:28:46 +0530

    Initial commit

---
+++ udp_client.c
@@ -0,0 +1,50 @@
+// Client side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#define PORT	 8080 
+#define MAXLINE 1024 
+
+// Driver code 
+int main() { 
+	int sockfd; 
+	char buffer[MAXLINE]; 
+	struct sockaddr_in	 servaddr; 
+
+	// Creating socket file descriptor 
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	
+	// Filling server information 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	
+	int n, len; 
+	printf("Enter message to send from client\n");
+	char *hello;
+	scanf("%s",hello);
+	sendto(sockfd, (const char *)hello, strlen(hello), 
+		MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+			sizeof(servaddr)); 
+	printf("%s message sent.\n",hello); 
+		
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+				MSG_WAITALL, (struct sockaddr *) &servaddr, 
+				&len); 
+	buffer[n] = '\0'; 
+	printf("Server : %s\n", buffer); 
+
+	close(sockfd); 
+	return 0; 
+} 
